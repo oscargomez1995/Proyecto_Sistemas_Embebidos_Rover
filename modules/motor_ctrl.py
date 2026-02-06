481174c51c5b5b8f0ab9799cad13cb928fb56e63
hsierrav21 <83846511+hsierrav21@users.noreply.github.com>
2026-02-06 04:43:30 -0600

    Update motor_ctrl.py

--- modules/motor_ctrl.py
+++ modules/motor_ctrl.py
@@ -1,27 +1,31 @@
-import RPi.GPIO as GPIO
 import time
+from pca9685 import PCA9685
 
 class ControlMotores:
-    def __init__(self, pins):
-        self.pins = pins
-        for pin in self.pins.values():
-            GPIO.setup(pin, GPIO.OUT)
-        self.pwm_izq = GPIO.PWM(self.pins['PWM_IZQ'], 1000)
-        self.pwm_der = GPIO.PWM(self.pins['PWM_DER'], 1000)
-        self.pwm_izq.start(0)
-        self.pwm_der.start(0)
+    def __init__(self):
+        self.pwm = PCA9685(0x40)
+        self.pwm.set_pwm_freq(50)
+        self.vel_ref = 0
 
-    def mover(self, vel_izq, vel_der):
-        """Control básico de movimiento"""
-        GPIO.output(self.pins['DIR_IZQ'], vel_izq > 0)
-        GPIO.output(self.pins['DIR_DER'], vel_der > 0)
-        self.pwm_izq.ChangeDutyCycle(abs(vel_izq))
-        self.pwm_der.ChangeDutyCycle(abs(vel_der))
+    def mover(self, d1, d2, d3, d4):
+        # Mapeo: 0,1 izq_sup | 3,2 izq_inf | 6,7 der_sup | 4,5 der_inf
+        self._set_wheel(0, 1, d1)
+        self._set_wheel(3, 2, d2)
+        self._set_wheel(6, 7, d3)
+        self._set_wheel(4, 5, d4)
+        self.vel_ref = d1
 
-    def frenar_suave(self, vel_actual):
-        """Reduce la velocidad gradualmente (Seguridad)"""
-        for v in range(vel_actual, -1, -5):
-            self.pwm_izq.ChangeDutyCycle(v)
-            self.pwm_der.ChangeDutyCycle(v)
-            time.sleep(0.02)
-        self.mover(0, 0)
+    def _set_wheel(self, p1, p2, duty):
+        if duty > 0:
+            self.pwm.set_motor_pwm(p1, 0); self.pwm.set_motor_pwm(p2, abs(duty))
+        elif duty < 0:
+            self.pwm.set_motor_pwm(p2, 0); self.pwm.set_motor_pwm(p1, abs(duty))
+        else:
+            self.pwm.set_motor_pwm(p1, 4095); self.pwm.set_motor_pwm(p2, 4095)
+
+    def frenar_suave(self):
+        for v in range(abs(self.vel_ref), -1, -500):
+            p = v if self.vel_ref > 0 else -v
+            self.mover(p, p, p, p)
+            time.sleep(0.05)
+        self.mover(0, 0, 0, 0)
